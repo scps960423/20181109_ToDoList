5ee153bf31be92c0c303b341610ff573f828fa2a
林韻如 <scps960423@gmail.com>
2019-11-09 23:47:03 +0800

    頁面切換&VueJS實作

--- index.php
+++ index.php
@@ -1,27 +1,24 @@
 <?php
 
 /*連線資料，點下新增時*/
- if($_POST['submit'])
- {
+if ($_POST['submit']) {
   //db host&port&sid + user & password
   $db = "oci:dbname=(description=(address=(protocol=tcp)(host=140.117.69.58)(port=1521))(connect_data=(sid=ORCL)));charset=utf8";
   $username = "Group17";
   $password = "group171717";
-try {
-  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
+  try {
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
     $dbh = new PDO($db, $username, $password);
     //下達INSERT INTO 的語法 
-    $query="INSERT INTO tb_group (TITLE) VALUES ('".$_POST['TITLE']."')"; 
-      $result = $dbh->prepare("$query");
-      $result->execute();
-      //header("location:http://localhost/Project1/list.php");
-    }
-catch (PDOException $e) {
-    return("DB connect Error!: $e->getMessage()");
+    $query = "INSERT INTO tb_group (TITLE) VALUES ('" . $_POST['TITLE'] . "')";
+    $result = $dbh->prepare("$query");
+    $result->execute();
+    //header("location:http://localhost/Project1/list.php");
+  } catch (PDOException $e) {
+    return ("DB connect Error!: $e->getMessage()");
     die();
+  }
 }
-
- }
 ?>
 
 <!DOCTYPE html>
@@ -33,20 +30,35 @@ catch (PDOException $e) {
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="/css/rect.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <title>你好清單</title>
+  <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <title>HiToDo</title>
   <script>
     var $groupTool = $('.baseContent .content .tool'),
-        $addBut = $groupTool.find('.addBut a'),
-        $input = $groupTool.find('.input');
-        
-        $addBut.click(function(e){
-            e.preventDefault();
-            console.log($(this)[0]);
-            $input.toggleClass('is-active');
-        });
+      $addBut = $groupTool.find('.addBut a'),
+      $input = $groupTool.find('.input');
+
+    $addBut.click(function(e) {
+      e.preventDefault();
+      console.log($(this)[0]);
+      $input.toggleClass('is-active');
+    });
+    var json_baseDate = [{
+        "name": "類別",
+        "page": true
+      },
+      {
+        "name": "我分享的清單",
+        "page": false
+      },
+      {
+        "name": "共享的清單",
+        "page": false
+      }
+    ];
   </script>
 </head>
+
 <body>
   <div class="layout">
     <div class="baseHeader">
@@ -58,76 +70,81 @@ catch (PDOException $e) {
         <div class="tool"><a href="#">Sign out</a></div>
       </div>
     </div>
-    <div class="baseFrame">
+    <div class="baseFrame" id="toDoList">
       <div class="baseMenu">
         <div class="list">
-          <ul>
-            <li><a href="#">類別</a></li>
-            <li><a href="#">我分享的清單</a></li>
-            <li><a href="#">共享的清單</a></li>
+          <ul v-for="(item,i) in list">
+            <li><a href="#" v-on:click.prevent="tab(i)">{{ item.name }}</a></li>
           </ul>
         </div>
       </div>
       <div class="baseContent">
-        <div class="title">類別</div>
-        <div class="content">
-          <div class="tool">
-            <div class="input"><input name="TITLE" type="text" id="TITLE"></div>
-            <div class="addBut"><a href="#"><input name="submit" type="submit" id="submit" value="新增"></a></div>
-          </div>
+        <div class="page" v-if="list[0].page==true">
+          <div class="title">{{list[0].name}}</div>
+          <div class="content">
+            <div class="tool">
+              <div class="input"><input name="TITLE" type="text" id="TITLE"></div>
+              <div class="addBut"><a href="#"><input name="submit" type="submit" id="submit" value="新增"></a></div>
+            </div>
 
-          <div class="list">
-            <ul>
-<?php
+            <div class="list">
+              <ul>
+                <?php
 
-  $db = "oci:dbname=(description=(address=(protocol=tcp)(host=140.117.69.58)(port=1521))(connect_data=(sid=ORCL)));charset=utf8";
-  $username = "Group17";
-  $password = "group171717";
+                $db = "oci:dbname=(description=(address=(protocol=tcp)(host=140.117.69.58)(port=1521))(connect_data=(sid=ORCL)));charset=utf8";
+                $username = "Group17";
+                $password = "group171717";
 
-try {
-  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
-  $dbh = new PDO($db, $username, $password);
+                try {
+                  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+                  $dbh = new PDO($db, $username, $password);
 
-  $query = "SELECT * FROM TB_GROUP";
-  $result = $dbh->prepare("$query");
-  $result->execute();
-  while ($row = $result->fetch(PDO::FETCH_ASSOC))  {
-    ?>
-              <li>
-                <div class="name"><?php echo $row["TITLE"];?></div>
-                <div class="tool">
-                  <a href="http://" class="edit">編輯</a>
-                  <a href="http://" class="delete">刪除</a>
-                </div>
-              </li>
-<?php
-  }
- 
-} catch (PDOException $e) {
-  return("DB connect Error!: $e->getMessage()");
-  die();
-}
+                  $query = "SELECT * FROM TB_GROUP";
+                  $result = $dbh->prepare("$query");
+                  $result->execute();
+                  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    ?>
+                    <li>
+                      <div class="name"><?php echo $row["TITLE"]; ?></div>
+                      <div class="tool">
+                        <a href="http://" class="edit">編輯</a>
+                        <a href="http://" class="delete">刪除</a>
+                      </div>
+                    </li>
+                <?php
+                  }
+                } catch (PDOException $e) {
+                  return ("DB connect Error!: $e->getMessage()");
+                  die();
+                }
 
-?>
-              <li>
-                <div class="name"><a href="http://">HOME</a></div>
-                <div class="tool">
-                  <a href="http://" class="edit">編輯</a>
-                  <a href="http://" class="delete">刪除</a>
-                </div>
-              </li>
-            </ul>
+                ?>
+                <li>
+                  <div class="name"><a href="http://">HOME</a></div>
+                  <div class="tool">
+                    <a href="http://" class="edit">編輯</a>
+                    <a href="http://" class="delete">刪除</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+        <div class="page" v-if="list[1].page==true">
+          <div class="title">{{list[1].name}}</div>
+          <div class="content"></div>
+        </div>
+        <div class="page" v-if="list[2].page==true">
+          <div class="title">{{list[2].name}}</div>
+          <div class="content"></div>
+        </div>
+
       </div>
     </div>
 
   </div>
+  <script src="./js/main.js"></script>
 </body>
 
 
 </html>
-
-   
-   
-    
