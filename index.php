d8515ad62fc18739eac00265dbabaf1e3c738064
林韻如 <scps960423@gmail.com>
2019-11-12 02:12:51 +0800

    新增展開群組名稱編輯功能

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <?php
-  include("Main.php");
-   
+include("Main.php");
 
-  
-  /*取session的NUO &UNAME數值 */
-  if (isset($_POST['submit_g_create'])) {
-    _GroupCreate($_SESSION['UNO'],$_POST['gtitle']);
-  }
 
-  if (!isset($_SESSION['UNO'])) {
-    header("Location:signIn.php");
- }
 
-  $uno =$_SESSION['UNO']; //uno
-  $uname = $_SESSION['UNAME']; //uname
-  $query = "SELECT u.*,g.title AS gtitle,l.title AS ltitle,l.createday AS lcreate,i.content,i.author,i.createday AS icreate,i.oncheck";
-  $query .= " FROM TB_USER u LEFT JOIN tb_group g ON u.uno=g.uno LEFT JOIN tb_list l ON g.uno=l.uno AND g.gno=l.gno LEFT ";
-  $query .= "JOIN tb_item i ON l.uno=i.uno AND l.gno=i.gno AND l.lno=i.lno WHERE u.UNO = '$uno'";
-  $stid = _SQLOpen($GLOBALS['db'],$query);
-  $resultJSON_own = json_encode($stid->fetchAll(PDO::FETCH_ASSOC)); //user自己的Group..到item的Json
-  // $json_string = json_encode($resultJSON_own, JSON_PRETTY_PRINT);
-  // echo $json_string;
-  // print_r($resultJSON_own);
+/*取session的NUO &UNAME數值 */
+if (isset($_POST['submit_g_create'])) {
+  _GroupCreate($_SESSION['UNO'], $_POST['gtitle']);
+}
 
-  $query = "SELECT s.*,l.title AS ltitle,l.createday AS lcreate,i.content,i.author,i.createday AS icreate,i.oncheck ";
-  $query .= "FROM TB_USER u ";
-  $query .= "LEFT JOIN tb_share s ON u.uno=s.sno ";
-  $query .= "LEFT JOIN tb_list l ON s.sno=l.uno AND s.gno=l.gno ";
-  $query .= "LEFT JOIN tb_item i ON l.uno=i.uno AND l.gno=i.gno AND l.lno=i.lno WHERE u.UNO = '$uno'";
-  $stid = _SQLOpen($GLOBALS["db"],$query);
-  $resultJSON_Share = json_encode($stid->fetchAll(PDO::FETCH_ASSOC));//user分享的list的Json
+if (!isset($_SESSION['UNO'])) {
+  header("Location:signIn.php");
+}
 
-  $query = "SELECT s.*,l.title AS ltitle,l.createday AS lcreate,i.content,i.author,i.createday AS icreate,i.oncheck ";
-  $query .= "FROM TB_USER u ";
-  $query .= "LEFT JOIN tb_share s ON u.uno=s.uno ";
-  $query .= "LEFT JOIN tb_list l ON s.sno=l.uno AND s.gno=l.gno ";
-  $query .= "LEFT JOIN tb_item i ON l.uno=i.uno AND l.gno=i.gno AND l.lno=i.lno WHERE u.UNO = '$uno'";
-  $stid = _SQLOpen($GLOBALS['db'],$query);
-  $resultJSON_BeShare = json_encode($stid->fetchAll(PDO::FETCH_ASSOC));//user被分享的list的Json
+$uno = $_SESSION['UNO']; //uno
+$uname = $_SESSION['UNAME']; //uname
+$query = "SELECT u.*,g.title AS gtitle,l.title AS ltitle,l.createday AS lcreate,i.content,i.author,i.createday AS icreate,i.oncheck";
+$query .= " FROM TB_USER u LEFT JOIN tb_group g ON u.uno=g.uno LEFT JOIN tb_list l ON g.uno=l.uno AND g.gno=l.gno LEFT ";
+$query .= "JOIN tb_item i ON l.uno=i.uno AND l.gno=i.gno AND l.lno=i.lno WHERE u.UNO = '$uno'";
+$stid = _SQLOpen($GLOBALS['db'], $query);
+$resultJSON_own = json_encode($stid->fetchAll(PDO::FETCH_ASSOC)); //user自己的Group..到item的Json
+// $json_string = json_encode($resultJSON_own, JSON_PRETTY_PRINT);
+// echo $json_string;
+// print_r($resultJSON_own);
+
+$query = "SELECT s.*,l.title AS ltitle,l.createday AS lcreate,i.content,i.author,i.createday AS icreate,i.oncheck ";
+$query .= "FROM TB_USER u ";
+$query .= "LEFT JOIN tb_share s ON u.uno=s.sno ";
+$query .= "LEFT JOIN tb_list l ON s.sno=l.uno AND s.gno=l.gno ";
+$query .= "LEFT JOIN tb_item i ON l.uno=i.uno AND l.gno=i.gno AND l.lno=i.lno WHERE u.UNO = '$uno'";
+$stid = _SQLOpen($GLOBALS["db"], $query);
+$resultJSON_Share = json_encode($stid->fetchAll(PDO::FETCH_ASSOC)); //user分享的list的Json
+
+$query = "SELECT s.*,l.title AS ltitle,l.createday AS lcreate,i.content,i.author,i.createday AS icreate,i.oncheck ";
+$query .= "FROM TB_USER u ";
+$query .= "LEFT JOIN tb_share s ON u.uno=s.uno ";
+$query .= "LEFT JOIN tb_list l ON s.sno=l.uno AND s.gno=l.gno ";
+$query .= "LEFT JOIN tb_item i ON l.uno=i.uno AND l.gno=i.gno AND l.lno=i.lno WHERE u.UNO = '$uno'";
+$stid = _SQLOpen($GLOBALS['db'], $query);
+$resultJSON_BeShare = json_encode($stid->fetchAll(PDO::FETCH_ASSOC)); //user被分享的list的Json
 
 ?>
 
@@ -87,7 +87,7 @@
       </div>
       <div class="moduleUser">
 
-        
+
         <div class="test">hi!<span><?php echo $_SESSION['UNAME']; ?> </span></div>
         <div class="tool"><a href="signIn.php">Sign out</a></div>
 
@@ -104,91 +104,93 @@
       <div class="baseContent">
         <div class="page" v-if="list[0].page==true">
           <div class="title">{{list[0].name}}</div>
-<!--新增開始-->
+          <!--新增開始-->
           <div class="content">
             <form action="DB_Insert.php" method="get">
-            <div class="tool">
-            <!--先給Uno固定給Session傳入的值，傳入DB_Insert做為值來新增欄位-->
-            <input type="Hidden" name="Insert_UNO" value="<?php echo $_SESSION['UNO'];?>">
-            <font>新的類別:</font>
-            <input type="text" name="Insert_TITLE">
-            <input type="hidden" name="group" value="Group_Insert">
-            <!--Q CSS套不到下面的Input上-->
-            <input type="submit" value="新增" >
-            </div>
-            </from>
-<!--新增結束-->
-            <div class="list">
-              <ul>
-                <?php
+              <div class="tool">
+                <!--先給Uno固定給Session傳入的值，傳入DB_Insert做為值來新增欄位-->
+                <input type="Hidden" name="Insert_UNO" value="<?php echo $_SESSION['UNO']; ?>">
+                <font>新的類別:</font>
+                <input type="text" name="Insert_TITLE">
+                <input type="hidden" name="group" value="Group_Insert">
+                <!--Q CSS套不到下面的Input上-->
+                <input type="submit" value="新增">
+              </div>
+              </from>
+              <!--新增結束-->
+              <div class="list">
+                <ul>
+                  <?php
 
-                $db = "oci:dbname=(description=(address=(protocol=tcp)(host=140.117.69.58)(port=1521))(connect_data=(sid=ORCL)));charset=utf8";
-                $username = "Group17";
-                $password = "group171717";
+                  $db = "oci:dbname=(description=(address=(protocol=tcp)(host=140.117.69.58)(port=1521))(connect_data=(sid=ORCL)));charset=utf8";
+                  $username = "Group17";
+                  $password = "group171717";
 
-                try {
-                  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-                  $dbh = new PDO($db, $username, $password);
+                  try {
+                    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+                    $dbh = new PDO($db, $username, $password);
 
-                  $query = "SELECT * FROM TB_GROUP WHERE UNO = '$uno'";
-                  $result = $dbh->prepare("$query");
-                  $result->execute();
-                  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    ?>
-                    <li>
-                         <!--傳值到LIST開始-->
-                        <div class="name"><a href="List/List.php?GNO=<?php  echo $row["GNO"];?> &
-                        										 UNO=<?php  echo $_SESSION['UNO'];?> ">
-                        <?php echo $row["TITLE"]; ?>										
-                        </a>
+                    $query = "SELECT * FROM TB_GROUP WHERE UNO = '$uno'";
+                    $result = $dbh->prepare("$query");
+                    $result->execute();
+                    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                      ?>
+                      <li>
+                        <!--傳值到LIST開始-->
+                        <div class="name"><a href="List/List.php?GNO=<?php echo $row["GNO"]; ?> &
+                        										 UNO=<?php echo $_SESSION['UNO']; ?> ">
+                            <?php echo $row["TITLE"]; ?>
+                          </a>
                         </div>
                         <!--傳值到LIST結束-->
                         <div class="tool">
-                        <!--編輯Start-->
-                        <form action="DB_UPDATE.php" method="get">
-                        <!--Hidden 要傳送的GNO & UNO-->
-                        <input type="Hidden" name="GNO" value="<?php  echo $row["GNO"];?>"> 
-                        <input type="Hidden" name="UNO" value="<?php  echo $row["UNO"];?>"> 
-                        <!--Q3：讓修改在點編輯的狀況下才出現輸入文字框-->
-                        <input type="text" name="NEW_TITLE">
-                        <!--提交按鈕型態為submit「傳送資料」-->
-                        <!--Q1：套用不上CSS-->
-                       <div class="edit">
-                    <input type="submit" value="修改">
-                    </div>
-                  </form>
-                  <!--編輯結束-->
-                        <a href="DB_Delete.php?GNO=<?php  echo $row["GNO"];?>"class="delete">刪除</a>
-                      </div>
-                    </li>
-                <?php
-                  }
-                } catch (PDOException $e) {
-                  return ("DB connect Error!: $e->getMessage()");
-                  die();
-                }
+                          <!--編輯Start-->
+                          <form action="DB_UPDATE.php" method="get">
+                            <!--Hidden 要傳送的GNO & UNO-->
+                            <input type="Hidden" name="GNO" value="<?php echo $row["GNO"]; ?>">
+                            <input type="Hidden" name="UNO" value="<?php echo $row["UNO"]; ?>">
+                            <!--Q3：讓修改在點編輯的狀況下才出現輸入文字框-->
+                            <div class="editCt">
+                              <input type="text" name="NEW_TITLE">
+                              <input type="submit" value="確定">
+                            </div>
 
-                ?>
-                <li>
-                  <div class="name"><a href="http://">HOME</a></div>
-                  <div class="tool">
-                    <a href="http://" class="edit">編輯</a>
-                    <a href="http://" class="delete">刪除</a>  
-                  </div>
-                </li>
-              </ul>
-            </div>
+                            <!--提交按鈕型態為submit「傳送資料」-->
+                            <!--Q1：套用不上CSS-->
+                            <a href="#" class="edit"></a>
+                          </form>
+                          <!--編輯結束-->
+                          <a href="DB_Delete.php?GNO=<?php echo $row["GNO"]; ?>" class="delete">刪除</a>
+                        </div>
+                      </li>
+                  <?php
+                    }
+                  } catch (PDOException $e) {
+                    return ("DB connect Error!: $e->getMessage()");
+                    die();
+                  }
+
+                  ?>
+                  <li>
+                    <div class="name"><a href="http://">HOME</a></div>
+                    <div class="tool">
+                      <a href="http://" class="edit">編輯</a>
+                      <a href="http://" class="delete">刪除</a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
           </div>
         </div>
 
-         <div class="page" v-if="list[1].page==true">
+        <div class="page" v-if="list[1].page==true">
           <div class="title">{{list[1].name}}</div>
           <div class="content"></div>
         </div>
         <div class="page" v-if="list[2].page==true">
           <div class="title">{{list[2].name}}</div>
           <div class="content">
-          	
+
 
           </div>
         </div>
